6235785e5bb873c55384b76ac7bb47069d38ec43
Moonsk313 <mouncef.guinou@idweaver.com>
2016-02-18 12:00:08 +0000

    first implementation of my server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.get("/", function(req,res,next){
+    res.send("Node at idweaver by Mouncef");
+});
+
+
+var port = process.env.PORT  || 3000;
+
+app.listen(port,function(err){
+    if (err) {
+        console.dir(err)
+    }
+    else{
+        console.log("The server is listening");
+    }
+});

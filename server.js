7b900530334cfe8434c1f020c6ce139f87921530
Moonsk313 <mouncef.guinou@idweaver.com>
2016-03-03 12:13:45 +0000

    added a view

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
 
 var app = express();
 
 app.use(express.static('public'));
 
+app.engine('hbs', exphbs(
+    {
+        defaultLayout: 'main',
+        extname:'.hbs'
+    }));
+
+app.set('View Engine','hbs')
+
 app.get("/", function(req,res,next){
-    res.send("Node at idweaver by Mouncef");
+       res.render('home');
 });
 
 module.exports = app;
